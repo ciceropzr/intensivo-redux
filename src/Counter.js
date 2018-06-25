b75bec0a61624cc5efb6735e58094f3acc5e1ddd
Juliana Bacelar <juliana.univasf@gmail.com>
2018-06-25 09:54:08 -0300

    Remove redux

--- src/Counter.js
+++ src/Counter.js
@@ -1,52 +1,29 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import {
-  increment,
-  decrement,
-  pause,
-  resume,
-  restart
-} from './module'
-
-const mapStateToProps = state => {
-  return {
-    count: state.count
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    increment: () => {
-      dispatch(increment())
-    },
-    decrement: () => {
-      dispatch(decrement())
-    },
-    pause: () => {
-      dispatch(pause())
-    },
-    resume: () => {
-      dispatch(resume())
-    },
-    restart: () => {
-      dispatch(restart())
-    }
-  }
-}
 
 class Counter extends React.Component {
+  state = { count: 0 }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
   render() {
     return (
       <div style={styles.container}>
         <h2>Counter</h2>
         <div>
-          <button onClick={this.props.decrement}>-</button>
-          <span>{this.props.count}</span>
-          <button onClick={this.props.increment}>+</button>
+          <button onClick={this.decrement}>-</button>
+          <span>{this.state.count}</span>
+          <button onClick={this.increment}>+</button>
         </div>
-        <button onClick={this.props.pause}>Pause</button>
-        <button onClick={this.props.resume}>Resume</button>
-        <button onClick={this.props.restart}>Restart</button>
       </div>
     )
   }
@@ -61,4 +38,4 @@ const styles = {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter)
+export default Counter
--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 // Libs
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-
-// Reducers
-import countReducer from './module'
 
 // Components
 import App from './App'
 
-const store = createStore(countReducer, window.devToolsExtension && window.devToolsExtension())
-ReactDOM.render(
-<Provider store={store}>
-  <App />
-</Provider>
-, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
