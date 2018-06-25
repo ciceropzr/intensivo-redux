c313f49137fcceb9235e23c8849bbfd084ab39c3
Juliana Bacelar <juliana.univasf@gmail.com>
2018-06-24 21:55:54 -0300

    Create counter

---
+++ src/Counter.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  state = { count: 0 }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        <h2>Counter</h2>
+        <div>
+          <button onClick={this.decrement}>-</button>
+          <span>{this.state.count}</span>
+          <button onClick={this.increment}>+</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: 200
+  }
+}
+
+export default Counter;
